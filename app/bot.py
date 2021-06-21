59996f63ad8656b2973cecbdb2361259726e0edc
Yohann Boniface <edhyjox@gmail.com>
2021-06-21 22:40:45 +0200

    Improved ascii art

--- app/bot.py
+++ app/bot.py
@@ -1,28 +1,37 @@
 from datetime import datetime
 
-import dotenv
+import discord
 from discord.ext import commands
+
+import dotenv
 from app.timer import time
 
 
 class Bot(commands.Bot):
 
-    def __init__(self, command_prefix, **options):
-        super().__init__(command_prefix, **options)
+    def __init__(self, command_prefix):
+        print(self)
+
+        super(Bot, self).__init__(
+            command_prefix=command_prefix,
+            intents=discord.Intents.default(),
+            case_insensitive=True,
+        )
+
+    def __repr__(self):
+        return '\n'.join(
+            (
+                r"██████╗  ██████╗     ██████╗  █████╗ ██╗   ██╗███████╗    ██████╗  ██████╗ ████████╗",
+                r"╚════██╗██╔═████╗    ██╔══██╗██╔══██╗╚██╗ ██╔╝██╔════╝    ██╔══██╗██╔═══██╗╚══██╔══╝",
+                r" █████╔╝██║██╔██║    ██║  ██║███████║ ╚████╔╝ ███████╗    ██████╔╝██║   ██║   ██║",
+                r" ╚═══██╗████╔╝██║    ██║  ██║██╔══██║  ╚██╔╝  ╚════██║    ██╔══██╗██║   ██║   ██║",
+                r"██████╔╝╚██████╔╝    ██████╔╝██║  ██║   ██║   ███████║    ██████╔╝╚██████╔╝   ██║",
+                r"╚═════╝  ╚═════╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝    ╚═════╝  ╚═════╝    ╚═╝"
+            )
+        )
 
     def run(self):
         time("start")
-
-        print(
-            '\n'.join((
-                r"   ______ ____      ______        __ ",
-                r"  /___  // __ \____/ / __/_____  / /_",
-                r"   /_  // / / / __  / __  / __ \/ __/",
-                r" ___/ // /_/ / /_/ / /_/ / /_/ / /_  ",
-                r"/____//\____/\____/_____/\____/\__/  "
-            ))
-        )
-
         super().run(self.token)
 
     @property
