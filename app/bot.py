048b3dcb3e1734b00ff89731442cb6e85fcfdbe9
Yohann Boniface <edhyjox@gmail.com>
2021-06-21 21:46:15 +0200

    Improved Logs

--- app/bot.py
+++ app/bot.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+import time
+
 import dotenv
 from discord.ext import commands
 
@@ -7,18 +10,21 @@ class Bot(commands.Bot):
     def __init__(self, command_prefix, **options):
         super().__init__(command_prefix, **options)
 
-    def run(self):
-        super().run(self.token)
-
     @property
     def token(self):
         return dotenv.dotenv_values(".env")['TOKEN']
 
+    async def on_connect(self):
+        self.log(f'Logged in as {self.user} after {time.perf_counter():,.3f}s')
+
+    async def on_ready(self):
+        self.log(f'Ready after {time.perf_counter():,.3f}s')
+
     @staticmethod
-    async def on_ready():
-        print("Connexion ready")
+    def log(*args):
+        print(f"[{datetime.now():%d/%b/%Y:%Hh %Mm %Ss}]", *args)
 
 
 def main():
     bot = Bot('>')
-    bot.run()
+    bot.run(bot.token)
