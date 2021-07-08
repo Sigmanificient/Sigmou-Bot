9f8e1a4f4b9b03f9dc87fd15dc3a04e0a769368e
Yohann Boniface <edhyjox@gmail.com>
2021-07-08 16:46:27 +0200

    moving &code from dev to info

--- app/components/info.py
+++ app/components/info.py
@@ -1,3 +1,6 @@
+from os import listdir
+
+from app.embeds import Embed
 from discord.ext import commands
 
 
@@ -8,6 +11,17 @@ class InfoCog(commands.Cog):
         """Link to bot instance."""
         self.client = client
 
+        # Preloading file content
+        self.files = {}
+        folders = (".", "app", "app/components")
+        for file, path in {
+            _f: path for path in folders for _f in listdir(path) if _f.endswith(".py")
+        }.items():
+            with open(f"{path}/{file}", encoding="utf-8") as f:
+                self.files[file] = f.read()
+
+        self.files['Total'] = "\n".join(self.files.values())
+
     @commands.command(
         name="ping",
         description="Return Bot Latency",
@@ -17,6 +31,21 @@ class InfoCog(commands.Cog):
         """Return Bot Latency."""
         await ctx.send(f"Pong ! `{self.client.latency * 1000:.3f}` ms")
 
+    @commands.command(name="code")
+    async def get_code_info(self, ctx):
+        items = "`%s` characters\n `%s` lines"
+
+        code_embed = Embed(
+            ctx=ctx,
+            title="Code Structure",
+            description=f"> This is the whole code structure of {self.client.user.name}!"
+        ).add_fields(
+            self.files,
+            map_title=lambda name: f"📝 {name}" if name != "Total" else "📊 Total",
+            map_values=lambda file: items % (f"{len(file):,}", len(file.splitlines())),
+        )
+
+        await ctx.send(embed=code_embed)
 
 def setup(client):
     client.add_cog(InfoCog(client))
