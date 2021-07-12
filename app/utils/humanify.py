1b2d6c38fa6823f6f952948f220309080aaac1e6
Yohann Boniface <edhyjox@gmail.com>
2021-07-12 20:17:58 +0200

    Factorizing code and adding docstrings

--- app/utils/humanify.py
+++ app/utils/humanify.py
@@ -1,20 +1,11 @@
 from typing import Dict, Union, List
 
-second_units: Dict[str, int] = {
-    's': 60,
-    'm': 60,
-    'h': 24,
-    'd': 365,
-}
-
-sub_second_unit: Dict[str, float] = {
-    'ms': 0.001,
-    'µs': 0.000001,
-    "ns": 0.000000001
-}
+second_units: Dict[str, int] = {'s': 60, 'm': 60, 'h': 24, 'd': 365}
+sub_second_unit: Dict[str, float] = {'ms': 1e-03, 'µs': 1e-06, "ns": 1e-09}
 
 
 def pretty_time(time: Union[int, float]) -> str:
+    """Format time to a human readable string."""
     if not time:
         return '0s'
 
@@ -42,6 +33,7 @@ def pretty_time(time: Union[int, float]) -> str:
 
 
 def pretty_time_small(seconds: Union[int, float]) -> str:
+    """Format a sub second time to a human readable string."""
     for unit, eq in sub_second_unit.items():
         if eq < seconds:
             return f"{seconds / eq:,.2f}{unit}"
