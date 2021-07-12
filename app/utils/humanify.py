f19a98ce8e6965700c48229e258054e5b6f87bc7
Yohann Boniface <edhyjox@gmail.com>
2021-07-12 20:14:53 +0200

    Improved Typing and improved functions

--- app/utils/humanify.py
+++ app/utils/humanify.py
@@ -1,30 +1,36 @@
-second_units = {
+from typing import Dict, Union, List
+
+second_units: Dict[str, int] = {
     's': 60,
     'm': 60,
     'h': 24,
     'd': 365,
 }
 
-sub_second_unit = {
+sub_second_unit: Dict[str, float] = {
     'ms': 0.001,
     'µs': 0.000001,
     "ns": 0.000000001
 }
 
 
-def pretty_time(time: float) -> str:
-    sup = int(time)
-    display_units = []
+def pretty_time(time: Union[int, float]) -> str:
+    if not time:
+        return '0s'
 
-    if sup:
-        for unit, equal in second_units.items():
-            sup, n = divmod(sup, equal)
+    display_units: List[str] = []
 
-            if n:
-                display_units.append(f'{n}{unit}')
+    for unit, equal in second_units.items():
+        time, n = divmod(time, equal)
 
-    if sup:
-        display_units.append(f'{sup}y')
+        if n:
+            display_units.append(f'{n}{unit}')
+
+        if not time:
+            break
+
+    else:
+        display_units.append(f'{time}y')
 
     if len(display_units) == 1:
         return display_units[0]
@@ -35,7 +41,9 @@ def pretty_time(time: float) -> str:
     return ', '.join(display_units) + f' & {end}'
 
 
-def pretty_time_small(seconds: float) -> str:
+def pretty_time_small(seconds: Union[int, float]) -> str:
     for unit, eq in sub_second_unit.items():
         if eq < seconds:
             return f"{seconds / eq:,.2f}{unit}"
+
+    return '0s'
