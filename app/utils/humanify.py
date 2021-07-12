937c5e86a33ebf0789a0c5c8d47ce4d2da727390
Yohann Boniface <edhyjox@gmail.com>
2021-07-12 20:02:09 +0200

    [fix] moving unit test outside of the module

--- app/utils/humanify.py
+++ app/utils/humanify.py
@@ -39,27 +39,3 @@ def pretty_time_small(seconds: float) -> str:
     for unit, eq in sub_second_unit.items():
         if eq < seconds:
             return f"{seconds / eq:,.2f}{unit}"
-
-
-def main():
-    assert pretty_time(1) == '1s'
-    assert pretty_time(10) == '10s'
-
-    assert pretty_time(60) == '1m'
-    assert pretty_time(3600) == '1h'
-
-    assert pretty_time(86400) == '1d'
-    assert pretty_time(31536000) == '1y'
-
-    assert pretty_time(99999999) == '3y, 62d, 9h, 46m & 39s'
-    assert pretty_time(31536001) == '1y & 1s'
-
-    assert pretty_time_small(0.1) == '100.00ms'
-    assert pretty_time_small(0.005) == '5.00ms'
-
-    assert pretty_time_small(0.0005) == '500.00µs'
-    assert pretty_time_small(0.0000005) == '500.00ns'
-
-
-if __name__ == '__main__':
-    main()
