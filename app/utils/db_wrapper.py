be5a4ab635d3f4623bc364c779008a4984557062
Yohann Boniface <edhyjox@gmail.com>
2021-08-04 23:59:52 +0200

    adding fetchone and post

--- app/utils/db_wrapper.py
+++ app/utils/db_wrapper.py
@@ -1,5 +1,5 @@
 from app.utils.logging import log
-from typing import Optional
+from typing import Optional, Tuple, Any
 
 import aiosqlite
 
@@ -12,6 +12,21 @@ class Database:
     async def init(cls):
         cls.__db = await aiosqlite.connect("app/db.sqlite")
         log.success("Connection to the sqlite database is established")
+
+    async def fetchone(
+        self, sql: str, *args: Tuple[Any], default=None
+    ) -> Optional[Any]:
+        """Return a row from the database or a default value."""
+        async with self.__db.execute(sql, args) as cur:
+            async for val in cur:
+                return val if len(val) > 1 else val[0]
+
+        return default
+
+    async def post(self, sql: str, *args: Tuple[Any]):
+        """Execute a query and commit."""
+        await self.__db.execute(sql, args)
+        await self.__db.commit()
 
 
 db: Database = Database()
