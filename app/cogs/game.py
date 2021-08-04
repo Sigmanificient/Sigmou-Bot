8c46890cecd72929d881b68e041b8aabede64e62
Yohann Boniface <edhyjox@gmail.com>
2021-08-05 00:48:55 +0200

    adding start command

--- app/cogs/game.py
+++ app/cogs/game.py
@@ -3,6 +3,7 @@ from typing import NoReturn
 from discord.ext import commands
 
 from app.bot import Bot
+from app.utils.db_wrapper import db
 from app.utils.embeds import Embed
 from app.utils.timed_ctx import TimedCtx
 
@@ -13,6 +14,31 @@ class GameCog(commands.Cog):
     def __init__(self, client: Bot):
         """Link to bot instance."""
         self.client: Bot = client
+
+    @commands.command(name="start")
+    async def start_command(self, ctx: TimedCtx):
+        user = await db.fetchone(
+            "select true from users where discord_id = ?", ctx.author.id
+        )
+
+        if user:
+            await ctx.send(
+                embed=Embed(ctx)(
+                    title="Error",
+                    description="You already have an account !"
+                )
+            )
+
+            return
+
+        await db.post("insert into users(discord_id) values (?)", ctx.author.id)
+
+        await ctx.send(
+            embed=Embed(ctx)(
+                title="Welcome !",
+                description="Your account has just been created !"
+            )
+        )
 
 
 def setup(client: Bot) -> NoReturn:
