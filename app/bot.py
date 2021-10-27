b829577dc5d50c2f8f06253306c989030748b77f
Sigmanificient <sigmanificient@gmail.com>
2021-10-27 13:01:49 -0700

    :fire: Remove last discord import from `bot.py`

--- app/bot.py
+++ app/bot.py
@@ -2,14 +2,11 @@ import os
 from typing import NoReturn, Optional
 
 import dotenv
-from discord import Activity, ActivityType, Colour
-from discord.ext import commands, tasks
-from discord_components import DiscordComponents
+from pincer import Client, Intents
+from pincer.exceptions import CogError
 
 from app.utils.db_wrapper import db
 from app.utils.logging import log
-from app.utils.timer import time
-from pincer import Client, Intents
 
 
 class Bot(Client):
@@ -17,19 +14,12 @@ class Bot(Client):
     def __init__(self, prefix: str):
         """Sigmanificient Bot wrapper."""
         super(Bot, self).__init__(
-            # case_insensitive=True,
-            # command_prefix=commands.when_mentioned_or(prefix),
-            # owner_ids=(812699388815605791, 856491941184536597),
             intents=Intents.all(),
             token=self.token
         )
 
-        # self._skip_check = lambda x, y: False
-        self.colour: Colour = Colour(0xCE1A28)
+        self.colour = 0xCE1A28
         self.base_prefix: str = prefix
-
-        # removing default help command for overriding
-        # self.remove_command('help')
         self.load_components()
 
     def load_components(self) -> NoReturn:
@@ -43,7 +33,7 @@ class Bot(Client):
         try:
             self.load_cog(f"app.cogs.{component_name}")
 
-        except commands.ExtensionFailed as error:
+        except CogError as error:
             log.error(
                 f"Could not load component '{component_name}' "
                 f"due to {error.__cause__}"
