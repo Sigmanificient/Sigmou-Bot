4500d0d7e19243e95cf1e41a6d3dcc901d1ce478
Sigmanificient <sigmanificient@gmail.com>
2021-10-30 22:27:14 +0200

    :sparkles: Using ` __main__` && `__init__`

---
+++ sigmou/__main__.py
@@ -0,0 +1,10 @@
+import logging
+from sigmou.bot import Bot
+from sigmou.utils.timer import time
+
+logging.basicConfig(level=logging.DEBUG)
+
+if __name__ == '__main__':
+    time("start", keep=True)
+    bot: Bot = Bot(prefix='=')
+    bot.run()
