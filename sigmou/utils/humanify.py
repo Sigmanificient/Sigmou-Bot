8217992d06256425a5bffed20e1a58ef993a2621
Yohann Boniface <edhyjox@gmail.com>
2022-05-18 08:13:09 +0200

    ⏪️ Revert codacy next

--- sigmou/utils/humanify.py
+++ sigmou/utils/humanify.py
@@ -34,11 +34,8 @@ def pretty_time(time: Union[int, float]) -> str:
 
 def pretty_time_small(seconds: Union[int, float]) -> str:
     """Format a sub second time to a human readable string."""
-    return next(
-        (
-            f"{seconds / eq:,.2f}{unit}"
-            for unit, eq in sub_second_unit.items()
-            if eq < seconds
-        ),
-        '0s',
-    )
+    for unit, eq in sub_second_unit.items():
+        if eq < seconds:
+            return f"{seconds / eq:,.2f}{unit}"
+
+    return '0s'
