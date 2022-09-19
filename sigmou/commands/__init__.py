6430a37278ecaca413a9d14d4f7cfbbbdcfbd70e
sigmanificient <edhyjox@gmail.com>
2022-09-19 22:02:35 +0200

    :fire: Remove Old Import

--- sigmou/commands/__init__.py
+++ sigmou/commands/__init__.py
@@ -2,15 +2,13 @@ from .dev import DevCommandsGroup
 from .info import InfoCommandsGroup
 from .leveling import LevelCommandsGroup
 from .moderation import ModerationCommandsGroup
-from .time import TimeCommandsGroup
 
 
 groups = (
     DevCommandsGroup,
     InfoCommandsGroup,
     LevelCommandsGroup,
-    ModerationCommandsGroup,
-    TimeCommandsGroup
+    ModerationCommandsGroup
 )
 
 __all__ = ('groups',)
